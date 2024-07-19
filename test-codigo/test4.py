bca716c3c46a9fe092c83f78d637707a05742be5
Ju4n-4lvaR3Z <mgjuanalvarez4life@gmail.com>
2024-07-18 22:19:40 -0400

    w

--- test-codigo/test4.py
+++
@@ -1,2 +0,0 @@
-a=int(input("-"))
-print("uno") if a==1 else print("cero") if a==0 else print("no se")
--- test-codigo/test6.py
+++
@@ -1,6 +0,0 @@
-import datetime as dt
-from datetime import timedelta
-delta=dt.datetime.now()+timedelta(hours=4)
-
-print(f"{delta.strftime("%Y-%m-%d")}T{delta.strftime("%H")}:{delta.minute}:00Z")
-print("2024-04-24T04:25:00Z")
